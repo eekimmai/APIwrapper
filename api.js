fb810dcfb4706ff6aab317256345ca977b642f8c
Mike <ekimmai@outlook.com>
2022-06-29 04:06:14 +0300

    minor changes

--- api.js
+++ api.js
@@ -1,44 +1,34 @@
-//Below is fetch function calling URL
-
 const api_url = 'https://cat-fact.herokuapp.com/facts';
-//var data = "";
-//var newArray = "";
 
 async function getData() {
-  const response = await fetch(api_url);
-  const data = await response.json();
-  const newArray = [...data];
+//getting data from api and destructuring it to identify by index
+const response = await fetch(api_url);
+const data = await response.json();
+const newArray = [...data];
 
- //gets value of user #
-//let userInputNum = document.getElementById('userNum');
- //sets error message if value not in the range
-//let errMessage = "Not in the range!!!"
+//gets value of user # and gives error message if out of range
+let userInputNum = document.getElementById('userNum').value;
+let errorMessage = "Not in the range!!!"
 
+//below is for number of facts user wants
 
-document.getElementById("two").addEventListener("click", twoFunc)
-function twoFunc () {
-    getData();
-    document.getElementById("buttonValue").innerHTML = newArray[2].text;
-}
-
-
-/*if (userInputNum >= 0 && userInputNum <= 20) {
-   //getData();
+if (userInputNum >= 0 && userInputNum <= 20) {
+   getData();
    document.getElementById("numValue").innerHTML = newArray[userInputNum].text;
  } else {
-   document.getElementById("numValue").innerHTML = errMessage;
- }*/
+   document.getElementById("numValue").innerHTML = errorMessage;
+}
 
 }
 
+/*async function twoFunc() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const newArray = [...data];
+//twoFunc();
+document.getElementById("buttonValue").innerHTML = newArray[2].text;
+}*/
 
-
-
-
-
-
-//var newArray = getData();
-//newArray = [...data];
 
 //console.log(data);
 
